d87e0cc35424b3b64211b8a47c21ca9ea2dcd84a
itsmedenver <68907210+itsmedenver@users.noreply.github.com>
2021-10-30 15:21:49 +0800

    Added request location permission in main

--- app/src/main/java/com/capstone/app/utrace_cts/MainActivity.kt
+++ app/src/main/java/com/capstone/app/utrace_cts/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.capstone.app.utrace_cts
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
 import com.capstone.app.utrace_cts.fragments.HomeFragment
@@ -9,7 +16,10 @@ import com.capstone.app.utrace_cts.fragments.NotificationsFragment
 import com.capstone.app.utrace_cts.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2
+
 class MainActivity : AppCompatActivity() {
+
 
     private val homeFragment = HomeFragment()
     private val profileFragment = ProfileFragment()
@@ -36,6 +46,56 @@ class MainActivity : AppCompatActivity() {
             true
         }
         Utils.startBluetoothMonitoringService(this)
+    }
+
+    // Check if permissions are granted
+
+    val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+    // Request permission from user
+    private fun requestLocationPermission() {
+        if (isLocationPermissionGranted) {
+            return
+        }
+        runOnUiThread {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Location permission required")
+            builder.setMessage("Starting from Android M (6.0), the system requires apps to be granted " +
+                    "location access in order to scan for BLE devices.")
+            builder.setCancelable(false)
+            //builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = ))
+            builder.setPositiveButton(android.R.string.ok) {dialog, which
+                ->                 requestPermission(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            }
+            builder.show()
+        }
+    }
+
+    private fun Activity.requestPermission(permission: String, requestCode: Int) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.firstOrNull() == PackageManager.PERMISSION_DENIED) {
+                    requestLocationPermission()
+                } else {
+
+                }
+            }
+        }
     }
 
     // replaceFragment: updates the fragment holder (view) given a fragment
