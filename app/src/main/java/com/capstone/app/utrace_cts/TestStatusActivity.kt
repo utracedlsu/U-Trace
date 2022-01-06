25e65f923cf6b6316b80504e9d7c35a53eaa1ffe
AdrianDonato <67679435+AdrianDonato@users.noreply.github.com>
2022-01-06 17:16:14 +0800

    Test status refresh button logic

--- app/src/main/java/com/capstone/app/utrace_cts/TestStatusActivity.kt
+++ app/src/main/java/com/capstone/app/utrace_cts/TestStatusActivity.kt
@@ -2,8 +2,11 @@ package com.capstone.app.utrace_cts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
 class TestStatusActivity : AppCompatActivity() {
 
@@ -13,6 +16,7 @@ class TestStatusActivity : AppCompatActivity() {
     private lateinit var tvTestDate: TextView
     private lateinit var tvTestID: TextView
     private lateinit var btn_updateTestStatus : Button
+    private lateinit var testID: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +29,14 @@ class TestStatusActivity : AppCompatActivity() {
         tvTestID = findViewById(R.id.tv_details_testID)
         btn_back_TestStatus = findViewById(R.id.btn_back_TestStatus)
         btn_updateTestStatus = findViewById(R.id.btn_updateTestStatus)
+        testID = Preference.getLastTestID(applicationContext)
 
         // set TextViews
         if(!Preference.getTestStatus(applicationContext).equals("")){
             val testDate = Preference.getLastTestDate(applicationContext)
             tvTestDateHeader.setText("As of $testDate")
             tvTestDate.setText("$testDate")
-            tvTestID.setText("${Preference.getLastTestID(applicationContext)}")
+            tvTestID.setText("$testID")
 
             //change true to positive eventually
             if(Preference.getTestStatus(applicationContext).equals("true")){
@@ -50,7 +55,45 @@ class TestStatusActivity : AppCompatActivity() {
         btn_back_TestStatus.setOnClickListener { finish() }
 
         btn_updateTestStatus.setOnClickListener {
-            // TODO: ???
+            refreshTestStatus()
         }
+    }
+
+    private fun refreshTestStatus(){
+        FirebaseFirestore.getInstance().collection("users").document(Preference.getFirebaseId(applicationContext))
+            .get().addOnCompleteListener { task ->
+                if(task.isSuccessful){
+                    val result = task.result
+                    val covidTests = result?.get("covid_tests") as ArrayList<HashMap<String, Object>>
+
+                    if(covidTests.size > 0){
+                        if(covidTests.last().get("testID").toString().equals(testID)){
+                            Toast.makeText(applicationContext, "No new test status was found at this time, please try again later.", Toast.LENGTH_SHORT).show()
+                        } else {
+                            Preference.putLastTestID(applicationContext, covidTests.last().get("testID").toString())
+                            Preference.putTestStatus(applicationContext, "${result?.get("covid_positive").toString()}")
+                            Preference.putLastTestDate(applicationContext, "${result?.getString("last_testdate")}")
+
+                            //refresh textViews
+                            val testDate = Preference.getLastTestDate(applicationContext)
+                            tvTestDateHeader.setText("As of $testDate")
+                            tvTestDate.setText("$testDate")
+                            tvTestID.setText("${Preference.getLastTestID(applicationContext)}")
+
+                            //change true to positive eventually
+                            if(Preference.getTestStatus(applicationContext).equals("true")){
+                                tvTestStatus.setText("You have been tested POSTIVE for COVID-19.")
+                            } else {
+                                tvTestStatus.setText("You have been tested NEGATIVE for COVID-19.")
+                            }
+
+                            Toast.makeText(applicationContext, "Test status has been refreshed.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(applicationContext, "Unable to check for test status at this time, please try again later.", Toast.LENGTH_SHORT).show()
+                    Log.d("TestStatusLog", "Unable to retrieve records at this time. ${task.exception?.message}")
+                }
+            }
     }
 }
